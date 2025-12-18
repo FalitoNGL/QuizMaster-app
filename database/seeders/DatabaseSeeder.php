6466e06156046ada3075ecd8bff114d028b3c102
Falito Eriano <falitoeriano17@gmail.com>
2025-12-18 14:24:41 +0700

    Falito Eriano Nainggolan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             QuizSeeder::class,
-            QuestionSeeder::class,
         ]);
     }
 }
